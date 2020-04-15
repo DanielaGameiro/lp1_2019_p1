00989094423fbc9dd4e7214083eaae953ea2d950
Daniela Gameiro <danielagameiro@live.com.pt>
2020-04-15 18:28:28 +0100

    Adicionar base do tabuleiro no programa

--- WolfAndSheep/Program.cs
+++ WolfAndSheep/Program.cs
@@ -8,13 +8,48 @@ namespace WolfAndSheep
     class Program
     {
         /// <summary>
-        /// Inicialização do jogo
+        /// Inicializar o jogo
         /// </summary>
         private static void Main()
         {
-            // Cria um Menu Inicial e chama a função Menu
+            // Criar um Menu Inicial e chamar a função Menu
             MenuInicial menuInicial = new MenuInicial();
             menuInicial.Menu();
+
+            // Criar e renderizar um tabuleiro 8x8
+            Tabuleiro tabuleiroNovo = new Tabuleiro (8,8);
+            Render(tabuleiroNovo);
+        }
+
+        /// <summary>
+        /// Renderizar o tabuleiro completo
+        /// </summary>
+        /// <param name="tabuleiroNovo">Tabuleiro completo</param>
+        private static void Render(Tabuleiro tabuleiroNovo)
+        {
+            // Parte de cima do tabuleiro
+            Console.WriteLine("__________________________________________\n" +
+            "__________________________________________");
+            //Console.WriteLine("__________________________________________");
+
+            // Loop da largura do tabuleiro
+            for (int x = 0; x < tabuleiroNovo.X; x++)
+            {
+                // Loop da altura do tabuleiro
+                for (int y = 0; y < tabuleiroNovo.Y; y++)
+                {
+                    // Separadores entre os quadrados do tabuleiro
+                    Console.Write("||");
+                    
+                    // Exibe os espaços vazios e os X's no tabuleiro
+                    Console.Write($" {tabuleiroNovo[x, y]} ");
+                }
+                // Lado direito do tabuleiro
+                Console.Write("||\n");           
+            }
+            // Parte de baixo do tabuleiro
+            Console.WriteLine("__________________________________________\n" +
+            "__________________________________________");
         }
     }
 }
---
+++ WolfAndSheep/Tabuleiro.cs
@@ -0,0 +1,57 @@
+﻿namespace WolfAndSheep
+{
+    /// <summary>
+    /// Esta classe contém o método Tabuleiro onde vai criar o tabuleiro 8x8
+    /// </summary>
+    public class Tabuleiro
+    {
+        // Declarar variáveis
+        private char[,] tabuleiro;
+
+        /// <summary>
+        /// Definir os valores do tabuleiro na posição escolhida
+        /// </summary>
+        /// <value>Posição X e Y</value>
+        public char this[int x, int y]
+        {
+            get { return tabuleiro[x, y]; }
+            set { tabuleiro[x, y] = value; }
+        }
+
+        // Dimensão X e Y do tabuleiro
+        public int X => tabuleiro.GetLength(0);
+        public int Y => tabuleiro.GetLength(1);
+
+        /// <summary>
+        /// Criar um tabuleiro com as dimensões escolhidas
+        /// </summary>
+        /// <param name="DimX">Largura</param>
+        /// <param name="DimY">Altura</param>
+        public Tabuleiro(int DimX, int DimY)
+        {
+            tabuleiro = new char[DimX, DimY];
+
+            // Largura 
+            for (int x = 0; x < DimX; x++)
+            {
+                // Altura
+                for (int y = 0; y < DimY; y++)
+                {
+                    // Verificar se a posição está num quadrado "não-jogável" 
+                    if (((x + y) % 2) == 0)
+                    {
+                        // Se sim, meter um X nesse quadrado
+                        tabuleiro[x, y] = 'X';
+                    }
+                    
+                    // Se a posição está num quadrado "jogável", deixar esse
+                    // espaço vazio
+                    else
+                    {
+                        tabuleiro[x, y] = ' ';
+                    }
+                }
+            }
+        }
+    }
+}
