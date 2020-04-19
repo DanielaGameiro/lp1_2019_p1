5d56a35ca4a03ef6d57581a257ae1cceee0bd497
João Borges <joaonunos.borges@gmail.com>
2020-04-19 03:33:51 +0100

    Criação de métodos adicionais na classe e desenvolvimento da movimentação do lobo e condição de derrota

---
+++ WolfAndSheep/WolfAndSheep/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace WolfAndSheep
+{
+    /// <summary>
+    /// Esta classe contém o método Main onde vai fazer loop do jogo
+    /// </summary>
+    class Program
+
+    {
+        /// <summary>
+        /// Inicializar o jogo
+        /// </summary>
+        private static void Main()
+        {
+            // Criar um Menu Inicial e chamar a função Menu
+            MenuInicial menuInicial = new MenuInicial();
+            menuInicial.Menu();
+
+            // Criar e renderizar um tabuleiro 8x8
+            Tabuleiro tabuleiroNovo = new Tabuleiro (8,8);
+            Render(tabuleiroNovo);
+
+            Wolf player1 = new Wolf(OvelhaDir);
+
+            while(!(player1.Loose() || player2.Loose()))
+            {
+                player1.Move();
+                player2.Move();
+            }
+            
+        }
+
+        /// <summary>
+        /// Renderizar o tabuleiro completo
+        /// </summary>
+        /// <param name="tabuleiroNovo">Tabuleiro completo</param>
+        private static void Render(Tabuleiro tabuleiroNovo)
+        {
+            // Parte de cima do tabuleiro
+            Console.WriteLine("__________________________________________\n" +
+            "__________________________________________");
+
+            // Loop da largura do tabuleiro
+            for (int x = 0; x < tabuleiroNovo.X; x++)
+            {
+                // Loop da altura do tabuleiro
+                for (int y = 0; y < tabuleiroNovo.Y; y++)
+                {
+                    // Separadores entre os quadrados do tabuleiro
+                    Console.Write("||");
+
+                    // Exibe os espaços vazios e os X's no tabuleiro
+                    Console.Write($" {tabuleiroNovo[x, y]} ");
+                }
+                // Lado direito do tabuleiro
+                Console.Write("||\n");
+            }
+            // Parte de baixo do tabuleiro
+            Console.WriteLine("__________________________________________\n" +
+            "__________________________________________");
+        }
+    }
+}
--- WolfAndSheep/WolfAndSheep/Wolf.cs
+++ WolfAndSheep/WolfAndSheep/Wolf.cs
@@ -1,86 +1,134 @@
 ﻿using System;
+using System.Linq;
 
 namespace WolfAndSheep
 {
    public class Wolf
     {
-        // Declarar variáveis
-        private int[] next_pos;
-        string[] separador;
-        string[] snext;
+        private int[] Pos;
 
 
-        // Wolf pos
-        public Wolf(int s)
+        public static void Start(int s)
         {
-            Pos = Start();
-        }
+            while (true)
+            {
+                int[] next_pos = new int[2];
+                Console.WriteLine("Player1 \n" + "Onde deseja começar o jogo?");
+                next_pos = Valid_move();
 
-        public int[] Pos {get;}
+                if (O == 1 && next_pos[1] == 8 && (next_pos[0] == 1 ||
+                next_pos[0] == 3 || next_pos[0] == 5 || next_pos[0] == 7))
+                {
+                    Pos = next_pos;
+                    return;
+                }
 
-        public static Array[] Start()
-        {
-            Console.WriteLine("Player1 \n" + "Onde deseja começar o jogo?");
-            
+                else if (O == 2 && next_pos[0] == 1 && (next_pos[1] == 2 ||
+                next_pos[1] == 4 || next_pos[1] == 6 || next_pos[1] == 8))
+                {
+                    Pos = next_pos;
+                    return;
+                }
+
+                else if (O == 3 && next_pos[1] == 1 && (next_pos[0] == 8 ||
+                next_pos[0] == 6 || next_pos[0] == 4 || next_pos[0] == 2))
+                {
+                    Pos = next_pos;
+                    return;
+                }
+                else if (O == 4 && next_pos[0] == 8 && (next_pos[1] == 1 ||
+                next_pos[1] == 3 || next_pos[1] == 5 || next_pos[1] == 7))
+                {
+                    Pos = next_pos;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Posição inválida");
+                }
+            }
         }
 
         public static void Move()
         {
-            Console.WriteLine("Para onde deseja mover o lobo?");
-            next_pos = Valid_move()
-
-            if (next_pos[0] - Wolf.pos[0] == 1 &&
-            next_pos[1] - Wolf.pos[1] == 1)
+            int[] next_pos = new int[2];
+            while(true)
             {
-                if (|Sheep.pos[n,0] - Wolf.pos[n,0]| == 1 &&
-                |Sheep.pos[n,1] - Wolf.pos[n,1]| == 1)
+                bool ocupada = false;
+                Console.WriteLine("Para onde deseja mover o lobo?");
+                next_pos = Valid_move();
+
+                if (next_pos[0] - Wolf.Pos[0] == 1 &&
+                next_pos[1] - Wolf.Pos[1] == 1)
                 {
-                    wolf.cant = [Sheep.pos[n,0], Sheep.pos[n,1]]
+                    for (int k = 0; k < 4; k++)
+                    {
+                        if (ovelhas[k].OvelhaX == next_pos[0] &&
+                        ovelhas[k].OvelhaY == next_pos[1])
+                        {
+                            Console.WriteLine("Posição ocupada");
+                            ocupada = true;
+                        }
+                    }
+
+                    if (ocupada == false)
+                    {
+                        Console.WriteLine($"Nova posição: {next_pos[0]},"+
+                        $"{next_pos[1]}");
+                        Pos = next_pos;
+                        return;
+                    }
                 }
-            }
-            else{
-                Console.WriteLine("Posição Inválida");
-            }
 
-
-            // Largura
-            for (int x = 0; x < DimX; x++)
-            {
-                // Altura
-                for (int y = 0; y < DimY; y++)
+                else
                 {
-                    // Verificar se a posição está num quadrado "não-jogável"
-                    if (((x + y)
-                    {
-                        if (sheep[x,y] == true)
-                        {
-                            tabuleiro[x, y] = 'S';
-                        }
-                        else if (Wolf[x,y] == true)
-                        {
-                            tabuleiro[x, y] = 'W';
-                        }
-                        else
-                        {
-                            // Se sim, meter um X nesse quadrado
-                            Wolf[x, y] = false;
-                        }
-
-                    }
-
-                    // Se a posição está num quadrado "jogável", deixar esse
-                    // espaço vazio
-                    else
-                    {
-                        tabuleiro[x, y] = ' ';
-                    }
+                    Console.WriteLine("Posição fora de alcance");
                 }
             }
         }
-        public static Array[] Valid_move()
+
+        public static void Loose()
         {
-            separador =  {",",".", ":", "/"};
-            snext = Console.ReadLine().Split(separador,
+            /// <summary>
+            /// Variável que determina os lugares bloqueados à volta do lobo
+            /// </summary>
+            int counter = 0;
+
+            /// <summary>
+            /// Quando o lobo se encontra numa das extremidades 2 movimentos
+            /// tornam-se impossíveis
+            /// </summary>
+            if (Pos[0] == 1 || Pos[0] == 8) counter += 2;
+            if (Pos[1] == 1 || Pos[1] == 8) counter += 2;
+
+            /// <summary>
+            /// Exceção para quando o lobo está num canto do tabuleiro
+            /// </summary>
+            if (counter == 4) counter = 3;
+
+            for (int k = 0; k < 4; k++)
+            {
+                if (Abs(ovelhas[k].OvelhaX - (Pos[0] - 1)) == 1 &&
+                Abs(ovelhas[k].OvelhaY - (Pos[1] - 1)) == 1)
+                {
+                    counter += 1;
+                }
+            }
+            /// <summary>
+            /// O lobo perde
+            /// </summary>
+            if (counter == 4) return true;
+
+            /// <summary>
+            /// O lobo não perde
+            /// </summary>
+            else return false;
+        }
+        public static Int32[] Valid_move()
+        {
+            int[] next_pos = new int[2];
+            char [] separador =  {' ',',', '.', ':', '/'};
+            string[] snext = Console.ReadLine().Split(separador,
             StringSplitOptions.RemoveEmptyEntries);
 
             while(true)
@@ -90,9 +138,10 @@ namespace WolfAndSheep
                     Console.WriteLine("Demasiadas indicações");
                     Console.WriteLine("Para onde deseja mover o lobo?");
                 }
+
                 else
                 {
-                    for(i = 0; i < 2; i++)
+                    for (i = 0; i < 2; i++)
                     {
                         if (!int.TryParse(snext[i], out next_pos[i]))
                         {
